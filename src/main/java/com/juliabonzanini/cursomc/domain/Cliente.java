9189fdb4d3ed83a7e83673ed6ac43a69aff6a68a
Julia Bonzanini <julia.bonzanini@gmail.com>
2025-04-05 00:17:55 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/com/juliabonzanini/cursomc/domain/Cliente.java
+++ src/main/java/com/juliabonzanini/cursomc/domain/Cliente.java
@@ -6,6 +6,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.juliabonzanini.cursomc.domain.enums.TipoCliente;
 
@@ -37,7 +38,7 @@ public class Cliente implements Serializable{
     @CollectionTable(name = "TELEFONE")
     private Set<String> telefones = new HashSet<>();
 
-    @JsonManagedReference
+    @JsonIgnore
     @OneToMany(mappedBy = "cliente")
     private List<Pedido> pedidos = new ArrayList<>();
 
--- src/main/java/com/juliabonzanini/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/juliabonzanini/cursomc/domain/PagamentoComBoleto.java
@@ -2,6 +2,7 @@ package com.juliabonzanini.cursomc.domain;
 
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.juliabonzanini.cursomc.domain.enums.EstadoPagamento;
 
 import jakarta.persistence.Entity;
@@ -10,7 +11,9 @@ import jakarta.persistence.Entity;
 public class PagamentoComBoleto extends Pagamento {
     private static final long serialVersionUID = 1L;
         
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date dataVencimento;
+    @JsonFormat(pattern = "dd/MM/yyyy HH:mm")
     private Date dataPagamento;
 
     public PagamentoComBoleto() {
